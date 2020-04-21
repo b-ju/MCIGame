56b52ded6635d31c5994b1ef22cf4633aa69857f
Ben Jurenka <58609154+b-ju@users.noreply.github.com>
2020-04-20 19:43:58 -0600

    Update GraphicsMethods.h

--- GraphicsMethods.h
+++ GraphicsMethods.h
@@ -1,6 +1,7 @@
 
 #pragma once
 #include "TrelGraphics2.h"
+//written by Trel J
 
 void drawScreen( std::vector<Brick> bricks, std::vector<Ball> balls, Paddle paddle, Level background );
 void drawTitleScreen( );
