15847a6a32ac5118b64e1d99cdb0a2a5c9c4f5cd
Ben Jurenka <58609154+b-ju@users.noreply.github.com>
2020-04-20 19:05:49 -0600

    Add files via upload

---
+++ GraphicsMethods_Current.h
@@ -0,0 +1,14 @@
+//
+//  GraphicsMethods_Current.hpp
+//  Test_Graphics
+//
+//  Created by ben jurenka on 4/20/20.
+//  Copyright © 2020 ben jurenka. All rights reserved.
+//
+#pragma once
+#include "TrelGraphics_Current.h"
+
+
+//void drawScreen( std::vector<Brick> bricks, std::vector<Ball> balls, Paddle paddle, Level background );
+
+//void drawTitleScreen( );
