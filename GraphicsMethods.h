c7d1262ddbf7399dacaf587642f38fdbd1a60256
Ben Jurenka <58609154+b-ju@users.noreply.github.com>
2020-04-22 13:48:50 -0600

    Delete GraphicsMethods.h

--- GraphicsMethods.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "TrelGraphics2.h"
-#include "Driver.h"
-//written by Trel J
-
-void drawFlatScreen( vector<Brick> bricks, vector<Ball> balls, Paddle paddle, Level background );
-void drawCircularScreen( std::vector<Brick> bricks, std::vector<Ball> balls, Paddle paddle, Level background ); // To be built at a later time
-void drawTitleScreen( );
